dd5f58467f9d99b7778bb2b9c3ad6aab604cb5b5
Nishil <nishilkapadia17@gmail.com>
2024-06-22 14:51:06 -0400

    chore: add three_sum refined

--- three_sum.ts
+++ three_sum.ts
@@ -26,4 +26,57 @@ function threeSum(nums: number[]): number[][] {
   return [...set.values()].map((x) => x.split(";").map((y) => parseInt(y)));
 }
 
-threeSum([-1, 0, 1, 2, -1, -4]);
+// Strategy 2: similar to the previous one, except pick a number, x and then sort the rest of the array
+// first sort the array
+// [-4, -1, -1, 0 , 1, 2]
+// then pick a number and implement the the two sum with the sorted array
+// without hashmap
+// 4
+// -1 2 = 1
+// -1 2 = 1
+// 0 2 = 2
+// 1 2 = 3
+
+// 1
+// -1 2 = 1
+// 0 2 = 2
+// 0 1 = 1
+
+// 1
+// 0 2 = 2
+// 1 2 = 3
+
+// do n^2 in o(1) space
+function threeSum_b(nums: number[]): number[][] {
+  const set = new Set<string>();
+  nums.sort((a, b) => a - b);
+  console.log(nums);
+  for (let i = 0; i < nums.length - 1; i++) {
+    const x = nums[i];
+    const target = 0 - x;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const total = nums[left] + nums[right];
+
+      if (total == target) {
+        set.add([x, nums[left], nums[right]].sort((a, b) => a - b).join(";"));
+        left++;
+
+        while (left < right && nums[left] == nums[right]) {
+          // another loop to remove the duplicate number
+          left++;
+        }
+      } else if (total > target) {
+        right--;
+      } else if (total < target) {
+        left++;
+      }
+    }
+  }
+
+  return [...set.values()].map((e) => e.split(";").map((x) => parseInt(x)));
+}
+
+console.log(threeSum_b([-1, 0, 1, 2, -1, -4]));
