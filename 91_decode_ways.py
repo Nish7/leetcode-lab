8479fae610eb01bc0afadfc8e8f48e46b53979eb
Nishil <nishil.kapadia@thescore.com>
2024-12-17 12:05:05 -0500

    fix: 91 decode ways

--- 91_decode_ways.py
+++ 91_decode_ways.py
@@ -4,10 +4,12 @@ class Solution:
         # Given dp[3] = 3. which means from s[0:3] we have 3 ways to decode
         # single digit check: Go dp[4], we can re-use dp[3] if s[4-1=3] not the 0. 
         # double digit check: also we can add dp[2] if s[2:4] if more than 10 and less than 26
+        if not s:
+            return 0
         dp = [0] * (len(s) + 1)
         
         dp[0] = 1 
-        dp[1] = 1 
+        dp[1] = 1 if s[0] != "0" else 0
         for i in range(2, len(s) + 1):
             if s[i-1] != "0":
                 dp[i] = dp[i-1]
